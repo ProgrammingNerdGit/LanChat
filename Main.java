a957905a9bd6d17edc162b5c399ff5a94bb93154
Programming Nerd <42655900+ProgrammingNerdGit@users.noreply.github.com>
2020-08-24 06:36:49 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,141 @@
+package com.gavin;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import  java.io.*;
+import java.net.URL;
+import java.util.*;
+import java.awt.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+class messages{
+    public static JLabel messages=new JLabel();
+}
+class runtime extends Thread {
+    static String serverIP = "";
+    public void init(String serverIPs){
+        serverIP = serverIPs;
+    }
+
+    public void run() {
+        try{
+            System.out.println ("Thread " +Thread.currentThread().getId() + " is running");
+            String Data = "";
+
+            while(true){
+
+                if(true){
+                    Process p = Runtime.getRuntime().exec("client.exe -getData "+serverIP);
+
+                    BufferedReader procInput = new BufferedReader(new
+                            InputStreamReader(p.getInputStream()));
+                    Data = procInput.readLine();
+                }
+                messages.messages.setText("<html>"+Data.replaceAll("%G2%12%99","<br/>")+"</html>");
+
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+public class Main {
+
+    static boolean connectedToServer = false;
+    static String serverIP = "";
+
+    public static void joinRoomWindow(){
+        JFrame frame = new JFrame("LanChat Join Room");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setBounds(0, 0, 300, 160);
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(new Color(18, 22, 49));
+
+        JTextField input = new JTextField("input url",16);
+        input.setBounds(0,0,150,25);
+
+        JButton join = new JButton("join");
+        join.setBounds(0,50,140,25);
+
+        join.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                connectedToServer = true;
+                serverIP = "http://"+input.getText();
+                frame.setVisible(false);
+                runtime thread = new runtime();
+                thread.init(serverIP);
+                thread.start();
+            }
+        });
+
+        frame.add(join);
+        frame.add(input);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) throws IOException, InterruptedException {
+        JFrame frame = new JFrame("LanChat");
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(0, 0, 500, 400);
+        frame.setLayout(null);
+        frame.getContentPane().setBackground(new Color(18, 22, 49));
+
+        JPanel panel=new JPanel();
+        panel.setBounds(40,30,445,290);
+        panel.setBackground(new Color(26, 46, 84));
+
+
+
+
+        messages.messages.setBounds(panel.getBounds());
+
+        messages.messages.setFont(new Font("Arial",Font.PLAIN,20));
+        messages.messages.setForeground(new Color(0, 214, 255));
+
+        JTextField input = new JTextField("input message",16);
+        input.setBounds(70,330,410,25);
+
+        JButton join = new JButton("join server");
+        join.setBounds(frame.getBounds().width/2-25,0,100,25);
+
+        JButton Send = new JButton("send");
+        Send.setBounds(5,330,65,25);
+
+        Send.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    sendData(input.getText());
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        join.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                joinRoomWindow();
+            }
+        });
+        frame.add(Send);
+        frame.add(messages.messages);
+        frame.add(input);
+        frame.add(panel);
+        frame.add(join);
+
+        frame.setVisible(true);
+
+    }
+
+    public static void sendData(String Data) throws IOException {
+        if(connectedToServer){
+            Process p = Runtime.getRuntime().exec("C:\\Users\\gavin\\OneDrive\\Desktop\\LanC\\Export\\client.exe -sendData "+serverIP+" "+Data.replaceAll(" ","%E2%96%88"));
+
+
+        }
+    }
+}
---
+++ client.py
@@ -0,0 +1,90 @@
+#2020 programming nerd
+
+
+import requests # module for geting / sending data from server
+from bs4 import BeautifulSoup # module for parseing data from server
+from http.server import HTTPServer, BaseHTTPRequestHandler # module for creating / managing server
+import threading # module used for useing threads
+import time # module used for delay
+import sys # module for connection with exturnal program
+
+data = '' # temperary veriable for parseing the data from the server
+
+timeBetwenRefresh = 0.2 # Time in second between each time the client asks for data from server
+
+class Serv(BaseHTTPRequestHandler):
+	def do_GET(self):
+		global data
+		if(self.path[:7] == "/?text="): # checks for message post request
+			data += "\n"+self.path.split("text=")[1] # adds sent data from client to global database of messages
+			pageData = open("format.html").read().replace("//data//",data.replace("%20"," ")) # formatting data for clients computer  
+			self.send_response(200) # sends validation response to server
+		elif(self.path[:5] == "/?get"): # checks for message get request
+			pageData = open("format.html").read().replace("//data//",data.replace("%20"," "))
+			self.send_response(200)
+		else:
+			pageData = "::404::"
+			self.send_response(404)# sends invalidation response to server
+
+		self.end_headers() # required http functon
+		self.wfile.write(bytes(pageData,'utf-8')) # sends data to client 
+	def log_message(self, format, *args):
+		return
+
+def serverInit(httpd_,ip,port):
+		httpd = httpd_
+		print("Server Started")
+		httpd.serve_forever()
+
+class server:
+	def __init__(self,ip,port):
+		self.httpd = HTTPServer((ip,int(port)),Serv) # var for HTTP server
+		self.x = threading.Thread(target=serverInit,args=(self.httpd,ip,port)) # 'serverInit' thread
+		self.ip = ip
+		self.port = port
+	def startServer(self):
+		self.x.start() # start 'serverInit' thread
+	def stopServer(self):
+		self.httpd.shutdown() # shutdown server
+
+
+
+
+class client:
+	def __init__(self,url):
+		self.url = url
+
+	def sendMessage(self,message):
+		try:
+			page = requests.get(str(self.url+"/?text="+message)) # "/?text=" used int url to send data to the server (used before the message)
+		except:
+			print("not valid ip/port")
+
+	def getMessages(self):
+		try:
+			page = requests.get(self.url+"/?get") # "/?get" used in url to get data without sending data
+			return BeautifulSoup(page.content,'lxml').find("div").text.replace("%E2%96%88"," ").replace("\n","%G2%12%99") # return data parsed and in a list (%E2%96%88 is space keycode)
+		except:
+			print("not valid ip/port")
+		
+		
+if(sys.argv[1] == "-startServ"): # checks for exturnal program asking to create a server
+	server = server(sys.argv[2],int(sys.argv[3])) # gets data from exturnal program and makes server (sys.argv[2]) = ip) (sys.argv[3]) = port)
+	server.startServer()
+
+
+if(sys.argv[1] == "-getData"): # checks for exturnal program asking to get data from a server
+	#print(sys.argv)
+	client = client(sys.argv[2]) #create client connection with data from ext. program (sys.argv[2]) = ip)
+	print(client.getMessages()) #get data from server
+
+if(sys.argv[1] == "-sendData"): # checks for exturnal program asking to send data from a server
+	client = client(sys.argv[2]) #create client connection with data from ext. program (sys.argv[2]) = ip)
+	client.sendMessage(sys.argv[3]) # sends data to server (sys.argv[3]) = message)
+	
+
+
+
+
+
+
